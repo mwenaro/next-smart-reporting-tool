516723fbd0588bfdbf5357d0551eaa076a33a0f5
Mwero Abdalla <mwerothewebmaker@gmail.com>
2023-12-16 20:14:28 +0300

    chore : srt initilized

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,22 @@
 
-import { HomePage } from "@/components";
+// import { HomePage } from "@/components";
 import { ToastDemo } from "@/components/atoms/ToastDemo";
+import { Button } from "@/components/ui/button";
 
 
 export default function Home() {
-  return <HomePage />
-  // return (
-  //   <div className="w-full flex flex-col gap-12 justify-center items-center h-full container">
-  //     <h2 className="font-extrabold text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-center ">
-  //       Hello, Welcome to NavTelema
-  //     </h2>
-  //     <ToastDemo title ={'Coming soon ...'}/>
-  //     {/* <Button className="font-semibold text-base md:text-2xl  md:py-8">
-  //       Coming soon ...
-  //     </Button> */}
-  //   </div>
-  // );
+  // return <HomePage />
+  return (
+    <div className="w-full flex flex-col gap-12 justify-center items-center h-full container">
+      <h2 className="font-extrabold text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-center ">
+        Hello, Welcome to SmartReportingTool (SRT)
+      </h2>
+      {/* <ToastDemo title ={'Coming soon ...'}/>
+      
+      */}
+      <Button className="font-semibold text-base md:text-2xl  md:py-8">
+        Coming soon ...
+      </Button>
+    </div>
+  );
 }
